4f59d91ab6b951527d84ba93ac15da29bbd47f7f
nbendasg <nick@bendas.info>
2025-05-15 16:22:08 -0400

    attemping alacritty custom config - getting messy

--- modules/terminal/alacritty.nix
+++ modules/terminal/alacritty.nix
@@ -93,9 +93,77 @@ in
     };
   };
 
-  # Desktop launcher and icon installation
+  # Ensure the config directory exists and write the config file
   home.activation = {
-    installAlacrittyDesktop = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    installAlacrittyConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ~/.config/alacritty
+      $DRY_RUN_CMD rm -f $VERBOSE_ARG ~/.config/alacritty/alacritty.yml
+      $DRY_RUN_CMD cat > $VERBOSE_ARG ~/.config/alacritty/alacritty.yml << EOF
+env:
+  TERM: xterm-256color
+
+window:
+  opacity: 0.9
+  decorations: full
+  dynamic_title: true
+  dynamic_padding: true
+  startup_mode: Maximized
+
+font:
+  size: 14
+  normal:
+    family: JetBrainsMono Nerd Font
+    style: Regular
+  bold:
+    family: JetBrainsMono Nerd Font
+    style: Bold
+  italic:
+    family: JetBrainsMono Nerd Font
+    style: Italic
+
+colors:
+  primary:
+    background: '0x0F1419'
+    foreground: '0xE6E1CF'
+  normal:
+    black: '0x0F1419'
+    red: '0xF07178'
+    green: '0xAAD94C'
+    yellow: '0xFFB454'
+    blue: '0x59C2FF'
+    magenta: '0xD2A6FF'
+    cyan: '0x95E6CB'
+    white: '0xE6E1CF'
+  bright:
+    black: '0x3E4B59'
+    red: '0xF07178'
+    green: '0xAAD94C'
+    yellow: '0xFFB454'
+    blue: '0x59C2FF'
+    magenta: '0xD2A6FF'
+    cyan: '0x95E6CB'
+    white: '0xF2F2F2'
+
+shell:
+  program: ${pkgs.zsh}/bin/zsh
+
+key_bindings:
+  - { key: V, mods: Control|Shift, action: Paste }
+  - { key: C, mods: Control|Shift, action: Copy }
+  - { key: Key0, mods: Control, action: ResetFontSize }
+  - { key: Equals, mods: Control, action: IncreaseFontSize }
+  - { key: Minus, mods: Control, action: DecreaseFontSize }
+
+scrolling:
+  history: 10000
+  multiplier: 3
+
+selection:
+  save_to_clipboard: true
+EOF
+    '';
+
+    installAlacrittyDesktop = lib.hm.dag.entryAfter ["installAlacrittyConfig"] ''
       $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ~/.local/share/applications
       $DRY_RUN_CMD rm -f $VERBOSE_ARG ~/.local/share/applications/alacritty.desktop
       $DRY_RUN_CMD cat > $VERBOSE_ARG ~/.local/share/applications/alacritty.desktop << EOF
