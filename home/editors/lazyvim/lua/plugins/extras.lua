639059801a89f456e0250c140b4e80892c94fdac
nbendasg <nick@bendas.info>
2025-05-16 15:04:41 -0400

    stuck on lazyvim extras, think this may work now

---
+++ home/editors/lazyvim/lua/plugins/extras.lua
@@ -0,0 +1,8 @@
+-- LazyVim extras configuration
+return {
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+} 
