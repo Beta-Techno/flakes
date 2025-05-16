0464c3b6bf990d523acc515b4aeb316f4497ed1c
nbendasg <nick@bendas.info>
2025-05-16 15:43:47 -0400

    stuck on lazyvim extras, think this may work now attempt 6

--- home/editors/lazyvim/lua/plugins/00-lazyvim.lua
+++ home/editors/lazyvim/lua/plugins/00-lazyvim.lua
@@ -1,3 +1,8 @@
 return {
-  { import = "lazyvim.plugins" },
+  {
+    "LazyVim/LazyVim",          --   the core plugin
+    import  = "lazyvim.plugins",--   must be first
+    version = false,            --   (optional) track main
+    opts    = { colorscheme = "tokyonight" },
+  },
 }
