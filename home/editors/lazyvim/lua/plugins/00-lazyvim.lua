93df52580b912a95c1095f6c09d14402baae1c26
nbendasg <nick@bendas.info>
2025-05-16 15:17:11 -0400

    stuck on lazyvim extras, think this may work now attemp 3

--- home/editors/lazyvim/lua/plugins/00-lazyvim.lua
+++ home/editors/lazyvim/lua/plugins/00-lazyvim.lua
@@ -1,7 +1,11 @@
--- LazyVim defaults override
 return {
   {
+    -- ① tell Lazy to pull in the core spec list
     "LazyVim/LazyVim",
+    import = "lazyvim.plugins",    -- ← THIS is the missing piece
+    version = false,               -- track the main branch (optional but handy)
+
+    -- ② any global LazyVim opts you want to override
     opts = {
       colorscheme = "tokyonight",
     },
