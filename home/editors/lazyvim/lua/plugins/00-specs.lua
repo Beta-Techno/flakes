0cd5b76fd12790ea5f9997ee0955e2a0a2928628
nbendasg <nick@bendas.info>
2025-05-16 16:14:47 -0400

    stuck on lazyvim extras, think this may work now attempt 10, not working

--- home/editors/lazyvim/lua/plugins/00-specs.lua
+++ home/editors/lazyvim/lua/plugins/00-specs.lua
@@ -1,11 +1,14 @@
 return {
-  -- 1. All extras
+  -- 1 · core
+  { import = "lazyvim.plugins" },
+
+  -- 2 · extras (add/remove as you like, order doesn't matter here)
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
-  -- 2. Finally, pull in custom plugins
+  -- 3 · your own plugins (everything under lua/plugins/**)
   { import = "plugins" },
 } 
