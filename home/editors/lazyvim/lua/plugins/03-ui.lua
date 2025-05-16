4e318dcb1ffc83a7df119a4478d7ecc9769b8272
nbendasg <nick@bendas.info>
2025-05-16 17:33:30 -0400

    removed custom buffer tabs

--- home/editors/lazyvim/lua/plugins/03-ui.lua
+++ home/editors/lazyvim/lua/plugins/03-ui.lua
@@ -61,42 +61,4 @@ return {
       },
     },
   },
-
-  -- Buffer line
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        mode = "buffers",
-        separator_style = "slant",
-        always_show_bufferline = true,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true,
-        show_tab_indicators = true,
-        show_bufferline = true,
-      },
-      highlights = {
-        separator = {
-          fg = "#073642",
-          bg = "#002b36",
-        },
-        separator_selected = {
-          fg = "#073642",
-        },
-        background = {
-          fg = "#657b83",
-          bg = "#002b36",
-        },
-        buffer_selected = {
-          fg = "#fdf6e3",
-          bold = true,
-        },
-        fill = {
-          bg = "#073642",
-        },
-      },
-    },
-  },
 }
