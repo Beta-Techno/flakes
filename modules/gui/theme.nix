ccc439d0c5aca7e6b4791454c3d0cfd26dc95408
nbendasg <nick@bendas.info>
2025-05-20 18:17:06 -0400

    added profile avatar attempt 2

--- modules/gui/theme.nix
+++ modules/gui/theme.nix
@@ -21,12 +21,23 @@ in {
     $DRY_RUN_CMD install -Dm644 -t ${config.xdg.dataHome}/backgrounds ${./../../assets/wallpapers/fish.jpeg}
   '';
 
-  # Set the user avatar
+  # Set the user avatar (both for session and login screen)
   home.file.".face" = {
     source = ./../../assets/icons/fish.png;
     onChange = ''
-      # Ensure the file is readable
       chmod 644 $out
     '';
   };
+
+  # Use AccountsService to set the avatar for GDM
+  home.activation.avatar = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    pic=${./../../assets/icons/fish.png}
+    uid=$(id -u)
+
+    # Ask AccountsService to copy & register the icon
+    busctl --system call org.freedesktop.Accounts \
+           /org/freedesktop/Accounts/User${uid} \
+           org.freedesktop.Accounts.User SetIconFile s "$pic" \
+      || echo "warning: could not set avatar"
+  '';
 }
