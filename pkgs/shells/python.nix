8e9db717be476df22e30f02150a1a7563c4a1a6f
nbendasg <nick@bendas.info>
2025-05-20 09:36:40 -0400

    nix cli 62 ~ attempt to remove circular dependencies

--- pkgs/shells/python.nix
+++ pkgs/shells/python.nix
@@ -17,6 +17,6 @@ pkgs.mkShell {
     python3Packages.pytest-cov  # Coverage
     python3Packages.ipython  # Interactive shell
     python3Packages.jupyter  # Notebooks
-    python-lsp-server  # LSP server
+    python3Packages.python-lsp-server  # LSP server
   ];
 } 
