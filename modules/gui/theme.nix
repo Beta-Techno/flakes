b690ef37601b8cad5f8cf3a2eefa3b5c74260b76
nbendasg <nick@bendas.info>
2025-05-20 18:11:28 -0400

    added profile avatar

--- modules/gui/theme.nix
+++ modules/gui/theme.nix
@@ -20,4 +20,13 @@ in {
   home.activation.copyWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
     $DRY_RUN_CMD install -Dm644 -t ${config.xdg.dataHome}/backgrounds ${./../../assets/wallpapers/fish.jpeg}
   '';
+
+  # Set the user avatar
+  home.file.".face" = {
+    source = ./../../assets/icons/fish.png;
+    onChange = ''
+      # Ensure the file is readable
+      chmod 644 $out
+    '';
+  };
 }
