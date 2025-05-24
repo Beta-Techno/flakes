1bf38176327d562499d1efb0d6ab975e9b651dde
nbendasg <nick@bendas.info>
2025-05-24 13:37:51 -0400

    deleted legacy

--- legacy/home/editors/lazyvim/lua/plugins/01-disable-mason.lua
+++
@@ -1,6 +0,0 @@
--- Disable Mason and anything that feeds it
-return {
-  { "williamboman/mason.nvim",          enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
-} 
