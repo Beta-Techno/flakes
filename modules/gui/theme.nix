ce44493925801e92db72c1ca1167d553396c3899
nbendasg <nick@bendas.info>
2025-09-02 23:15:14 -0400

    fixed xdc config conflict

--- modules/gui/theme.nix
+++ modules/gui/theme.nix
@@ -6,7 +6,7 @@ let
   themeBase = "Yaru-${accent}";
   gtkTheme = themeBase + lib.optionalString preferDark "-dark";
 in {
-  # Set the font through dconf
+  # Set the font through dconf (system-level)
   dconf.settings."org/gnome/desktop/interface" = {
     font-name = "JetBrainsMono Nerd Font 11";
     monospace-font-name = "JetBrainsMono Nerd Font 11";
@@ -16,20 +16,21 @@ in {
     icon-theme = themeBase;
   };
 
-  # Set the background
+  # Set the background (system-level, using absolute path)
   dconf.settings."org/gnome/desktop/background" = {
-    picture-uri = "file://${config.xdg.dataHome}/backgrounds/fish.jpeg";
-    picture-uri-dark = "file://${config.xdg.dataHome}/backgrounds/fish.jpeg";
+    picture-uri = "file:///home/nbg/.local/share/backgrounds/fish.jpeg";
+    picture-uri-dark = "file:///home/nbg/.local/share/backgrounds/fish.jpeg";
     picture-options = "zoom";
   };
-
-  home.activation.copyWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD mkdir -p ${config.xdg.dataHome}/backgrounds
-    $DRY_RUN_CMD cp -f ${./../../assets/wallpapers/fish.jpeg} ${config.xdg.dataHome}/backgrounds/fish.jpeg
-  '';
 
   # Set the user avatar (both for session and login screen)
   home.file.".face" = {
     source = ./../../assets/icons/fish.png;
   };
+
+  # Copy wallpaper to user directory
+  home.activation.copyWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p ~/.local/share/backgrounds
+    $DRY_RUN_CMD cp -f ${./../../assets/wallpapers/fish.jpeg} ~/.local/share/backgrounds/fish.jpeg
+  '';
 }
