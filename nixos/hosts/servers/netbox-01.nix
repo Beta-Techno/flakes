436fad638d85dea7868f4c3f8cb1344ef78434bd
nbendasg <nick@bendas.info>
2025-09-07 21:44:22 -0400

    refactor for servers. fix boot, attempt 4

--- nixos/hosts/servers/netbox-01.nix
+++ nixos/hosts/servers/netbox-01.nix
@@ -9,19 +9,16 @@
   networking.defaultGateway = "10.0.0.1";
   networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
 
-  # Disk layout (if using disko)
-  disko.devices = (import ../../disko/netbox-01.nix { inherit lib; }).disko.devices;
-
-  # Bootloader: Proxmox handles boot directly from disk
-  # Override base.nix bootloader configuration for VM
+  # Bootloader: BIOS GRUB for Proxmox VMs (like nick-vm)
   boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.grub.enable = lib.mkForce false;  # Disable grub as well
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
-  boot.loader.efi.efiSysMountPoint = lib.mkForce null;  # Don't try to mount /boot 
-  
-  # Disable bootloader requirement for VM environment
-  # Proxmox handles boot directly from disk
-  boot.loader.grub.devices = lib.mkForce [ ];  # Empty list disables grub requirement
+
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = false;        # BIOS mode
+    devices = [ "/dev/sda" ];  # whole disk in Proxmox
+    useOSProber = false;
+  };
 
   # Pin system state version
   system.stateVersion = "24.11";
