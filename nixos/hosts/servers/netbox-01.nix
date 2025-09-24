7bf473bf067e4ef00f276f0176bd9188c3c9dbfc
nbendasg <nick@bendas.info>
2025-09-24 19:50:16 -0400

    sops integration: update storage server ip

--- nixos/hosts/servers/netbox-01.nix
+++ nixos/hosts/servers/netbox-01.nix
@@ -6,7 +6,7 @@
   networking.useDHCP = true;
 
   # Reach storage-01 by name even on DHCP
-  networking.hosts."10.1.10.72" = [ "storage-01" ];
+  networking.hosts."10.1.10.73" = [ "storage-01" ];
 
   # Filesystems (UEFI needs a real ESP mounted at /boot)
   fileSystems."/" = {
