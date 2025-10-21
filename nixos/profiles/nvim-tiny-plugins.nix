100ae9736bb39296db2b30b099a7d1df393b40a4
nbendasg <nick@bendas.info>
2025-10-21 18:10:35 -0400

    Fix Neovim plugins configuration
    - Use configure.packages.myVimPackage.start instead of plugins option
    - Add plugins to start list (loaded automatically)
    - Use standard NixOS neovim module approach for plugins
    - Fixes 'option does not exist' error for plugins

--- nixos/profiles/nvim-tiny-plugins.nix
+++ nixos/profiles/nvim-tiny-plugins.nix
@@ -1,10 +1,15 @@
 # nixos/profiles/nvim-tiny-plugins.nix
 { pkgs, ... }:
 {
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    vim-sensible      # sane defaults
-    vim-surround      # change surroundings easily
-    vim-commentary    # gc to comment
-    vim-fugitive      # :G status, blame, etc.
-  ];
+  programs.neovim.configure = {
+    packages.myVimPackage = {
+      start = with pkgs.vimPlugins; [
+        vim-sensible      # sane defaults
+        vim-surround      # change surroundings easily
+        vim-commentary    # gc to comment
+        vim-fugitive      # :G status, blame, etc.
+      ];
+      opt = [ ];
+    };
+  };
 }
